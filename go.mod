60f8550b4d7c0ec5e85237628bdc44cc71fc2e4c
AyCarlito <daniel.hislop@mediakind.com>
2025-06-23 22:42:06 +0100

    feat: Generate directed graph

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/AyCarlito/go-mod-visualization
 
 go 1.23
+
+require (
+	github.com/spf13/cobra v1.8.1
+	go.uber.org/zap v1.27.0
+	golang.org/x/mod v0.13.0
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
---
+++ pkg/visualizer/graph.go
@@ -0,0 +1,130 @@
+package visualizer
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"strings"
+	"text/template"
+
+	"golang.org/x/mod/semver"
+)
+
+//go:embed graph.dot.tmpl
+var dotTemplate string
+
+//go:embed graph.html.tmpl
+var htmlTemplate string
+
+// Edge is a connection beteeen two nodes.
+type Edge struct {
+	Src string
+	Dst string
+}
+
+// Dependency is a dependency in a module.
+type Dependency struct {
+	Path    string
+	Version string
+}
+
+// Graph is a heirarchical representation of dependencies in a Go module.
+type Graph struct {
+	// Root is the module being represented.
+	Root string
+
+	// Edges are the edges in the graph.
+	Edges []Edge
+
+	// Selected are the dependencies included when the Go module is built.
+	// It maps a module path to its latest version.
+	Selected map[string]string
+
+	// Unselected are the dependencies that are not included when the Go module is built.
+	Unselected []Dependency
+
+	// format is the output format of the graph.
+	format string
+}
+
+// newGraph returns a new *graph.
+func newGraph(format string) *Graph {
+	return &Graph{
+		Selected: make(map[string]string),
+		format:   format,
+	}
+}
+
+// AddEdge adds an Edge to the Graph.
+func (g *Graph) AddEdge(src, dst string) {
+	g.Edges = append(g.Edges, Edge{
+		Src: src,
+		Dst: dst,
+	})
+}
+
+// AddNode adds a node to the Graph.
+func (g *Graph) AddNode(node string) {
+	// Extract the path version of the node.
+	// Root node has no version.
+	var path, version string
+	if i := strings.Index(node, "@"); i < 0 {
+		g.Root = node
+		return
+	} else {
+		path = node[:i]
+		version = node[i+1:]
+	}
+
+	// If haven't already encountered this path, this version is necessarily the latest version.
+	if latestVersion, ok := g.Selected[path]; !ok {
+		g.Selected[path] = version
+	} else {
+		// Already stored.
+		if version == latestVersion {
+			return
+		}
+		if semver.Compare(version, latestVersion) > 0 {
+			// This version is now the latest version.
+			// Store it, and add the previous latest version as an unselected dependency.
+			g.Selected[path] = version
+			g.Unselected = append(g.Unselected, Dependency{
+				Path:    path,
+				Version: latestVersion,
+			})
+		} else {
+			// Existing latest version is still the latest version.
+			// Store this version as an unselected dependency.
+			g.Unselected = append(g.Unselected, Dependency{
+				Path:    path,
+				Version: version,
+			})
+		}
+	}
+}
+
+// String returns the string representation of the Graph.
+func (g *Graph) String() (string, error) {
+	var outputTemplate string
+	switch g.format {
+	case "dot":
+		outputTemplate = dotTemplate
+	case "html":
+		outputTemplate = htmlTemplate
+	default:
+		return "", fmt.Errorf("invalid output format: %s", g.format)
+	}
+
+	t, err := template.New("graph").Parse(outputTemplate)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse template: %v", err)
+	}
+
+	var tpl bytes.Buffer
+	err = t.Execute(&tpl, g)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute template: %v", err)
+	}
+
+	return tpl.String(), nil
+}
